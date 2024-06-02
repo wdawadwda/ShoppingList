ef12f59e86251e70db8323bb0beb791a205f0c5e
Dzianis Bychuk <dzianis.bychuk.dev@gmail.com>
2024-06-02 15:40:33 +0300

    feat: linter and pritter

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+  const presets = [["babel-preset-expo"], ["@babel/preset-react", { runtime: "automatic" }]];
   return {
-    presets: ['babel-preset-expo'],
+    presets,
   };
 };
